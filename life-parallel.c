d0b88b90ede068af6577401af02486aed8a87e43
Sophie S <vpq5kd@virginia.edu>
2024-11-17 19:40:11 -0500

    started working on helper function for running each thread

--- life-parallel.c
+++ life-parallel.c
@@ -1,46 +1,24 @@
 #include "life.h"
 #include <pthread.h>
 
+void check_cells(int  height_start, int height_end, int width_start, int width_end){
 
-void simulate_life_run(LifeBoard *state, int steps){
-	LifeBoard *next_state = LB_new(state->width, state->height);
+	for (height_start; y < height_end ; y += 1) {
+    	for (width_start; x < state->width_end; x += 1) {
 
-		for (int step = 0; step < steps; step += 1) {
 
-			/* We use the range [1, width - 1) here instead of
-			 * [0, width) because we fix the edges to be all 0s.
-			 */
-			for (int y = 1; y < state->height - 1; y += 1) {
-				for (int x = 1; x < state->width - 1; x += 1) {
-
-					/* For each cell, examine a 3x3 "window" of cells around it,
-					 * and count the number of live (true) cells in the window. */
-					int live_in_window = 0;
-					for (int y_offset = -1; y_offset <= 1; y_offset += 1)
-						for (int x_offset = -1; x_offset <= 1; x_offset += 1)
-							if (LB_get(state, x + x_offset, y + y_offset))
-								live_in_window += 1;
-
-					/* Cells with 3 live neighbors remain or become live.
-					   Live cells with 2 live neighbors remain live. */
-					LB_set(next_state, x, y,
-						live_in_window == 3 /* dead cell with 3 neighbors or live cell with 2 */ ||
-						(live_in_window == 4 && LB_get(state, x, y)) /* live cell with 3 neighbors */
-					);
-				}
-			}
-
-			/* now that we computed next_state, make it the current state */
-			LB_swap(state, next_state);
 		}
-		LB_del(next_state);
-
-
-}
-
+	}
+};
 
 void simulate_life_parallel(int threads, LifeBoard *state, int steps) {
     /* YOUR CODE HERE */
+	
+	pthread_t threads[threads];
 
-
+	LifeBoard *next_state = LB_new(state->width, state->height);
+	for (int step = 0; step < steps; step += 1){
+		
+	}
+	
 }
