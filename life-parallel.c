5db4309de85b20763a2e9391d230980130196d32
Sophie S <vpq5kd@virginia.edu>
2024-11-19 03:54:00 -0500

    refactored code to only create 'threads' threads, created an array of arguments to ensure memory continutiy over threads

--- life-parallel.c
+++ life-parallel.c
@@ -8,30 +8,33 @@ typdef struct function_arguments(){
 	int width_end;
 	LifeBoard * args_state;
 	LifeBoard * args_next_state;
+	pthread_barrier_t * barrier;
 } arguments;
 
 void check_cells(void * args){
 
 	arguments * cc_args = (arguments *) args;
+	while(1){
+		for (int y = cc_args->height_start; y < cc_args->height_end ; y += 1) {
+			for (int x = cc_args->width_start; x < cc_args->width_end; x += 1) {
+				 /* For each cell, examine a 3x3 "window" of cells around it,
+					 * and count the number of live (true) cells in the window. */
+					int live_in_window = 0;
+					for (int y_offset = -1; y_offset <= 1; y_offset += 1)
+						for (int x_offset = -1; x_offset <= 1; x_offset += 1)
+							if (LB_get(cc_args->args_state, x + x_offset, y + y_offset))
+								live_in_window += 1;
 
-	for (int y = cc_args->height_start; y < cc_args->height_end ; y += 1) {
-    	for (int x = cc_args->width_start; x < cc_args->width_end; x += 1) {
-			 /* For each cell, examine a 3x3 "window" of cells around it,
-                 * and count the number of live (true) cells in the window. */
-                int live_in_window = 0;
-                for (int y_offset = -1; y_offset <= 1; y_offset += 1)
-                    for (int x_offset = -1; x_offset <= 1; x_offset += 1)
-                        if (LB_get(cc_args->args_state, x + x_offset, y + y_offset))
-                            live_in_window += 1;
+					/* Cells with 3 live neighbors remain or become live.
+					   Live cells with 2 live neighbors remain live. */
+					LB_set(cc_args->args_next_state, x, y,
+						live_in_window == 3 /* dead cell with 3 neighbors or live cell with 2 */ ||
+						(live_in_window == 4 && LB_get(cc_args->args_state, x, y)) /* live cell with 3 neighbors */
+					);
 
-                /* Cells with 3 live neighbors remain or become live.
-                   Live cells with 2 live neighbors remain live. */
-                LB_set(cc_args->args_next_state, x, y,
-                    live_in_window == 3 /* dead cell with 3 neighbors or live cell with 2 */ ||
-                    (live_in_window == 4 && LB_get(cc_args->args_state, x, y)) /* live cell with 3 neighbors */
-                );
-
+			}
 		}
+		pthread_barrier_wait(cc_args -> barrier);
 	}
 }
 
@@ -41,45 +44,46 @@ void simulate_life_parallel(int threads, LifeBoard *state, int steps) {
 	pthread_t check_threads[threads];
 
 	LifeBoard *next_state = LB_new(state->width, state->height);
-	
+
+	arguments args[threads];	
 	int width_sep = state->width/threads;
 	int height_sep = state->height/threads;
 
 	int width_remainder = state->width%threads;
 	int height_remainder = state->height%threads;
-
-	
-	for (int step = 0; step < steps; step += 1){
-
-		int hold_height_start = 0;
-		int hold_width_start = 0;
-		for (int thread_num = 0; thread_num < threads; thread_num +=1){
-			arguments args;
-			if (thread_num < (threads-1)){	
-				
-				args.height_start = hold_height_start;
-				args.height_end = (hold_height_start + height_sep) - 1;
-				args.width_start = hold_width_start;
-				args.width_end = (hold_width_end + width_sep) - 1;
-				
-				hold_height_start = args.height_end + 1;
-				hold_width_start = args.width_end + 1;
+       
+	for (int thread_num = 0; thread_num < threads; thread_num +=1){
+		if (thread_num < (threads-1)){	
 			
-			} else{
-				
-				args.height_start = hold_height_start;
-				args.height_end = (hold_height_start + height_remainder) -1;
-				args.width_start = hold_width_start;
-				args.width_end = (hold_width_start + width_remainder) -1;
-			}
-
-			args.args_state = state;
-			args.args_next_state = next_state;
-
+			args[thread_num].height_start = hold_height_start;
+			args[thread_num].height_end = (hold_height_start + height_sep) - 1;
+			args[thread_num].width_start = hold_width_start;
+			args[thread_num].width_end = (hold_width_end + width_sep) - 1;
 			
-
+			hold_height_start = args[thread_num].height_end + 1;
+			hold_width_start = args[thread_num].width_end + 1;
+		
+		} else{
 			
-		}		
+			args[thread_num].height_start = hold_height_start;
+			args[thread_num].height_end = (hold_height_start + height_remainder) -1;
+			args[thread_num].width_start = hold_width_start;
+			args[thread_num].width_end = (hold_width_start + width_remainder) -1;
+		}
+
+		args[thread_num].args_state = state;
+		args[thread_num].args_next_state = next_state;
+
+		
+		pthread_create(&check_threads[thread_num], NULL, check_cells, args[thread_num]);		
+
+		
+	}
+	for (int step = 0; step < steps; step +=1 ){
+		
+        /* now that we computed next_state, make it the current state */
+        LB_swap(state, next_state);		
 	}
 	
+    LB_del(next_state);
 }
