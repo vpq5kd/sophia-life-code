8284b7e29e0608c5bb21cba2f727a26319ef8b75
Sophie S <vpq5kd@virginia.edu>
2024-11-18 22:20:55 -0500

    added the cell visit code, refactored it to work with the struct variables

--- life-parallel.c
+++ life-parallel.c
@@ -7,13 +7,28 @@ void check_cells(void * args){
 		int height_end;
 		int width_start;
 		int width_end;
+		LifeBoard * args_state;
+		LifeBoard * args_next_state;
 	} arguments;
 
 	arguments * cc_args = (arguments *) args;
 
 	for (int y = cc_args->height_start; y < cc_args->height_end ; y += 1) {
     	for (int x = cc_args->width_start; x < cc_args->width_end; x += 1) {
+			 /* For each cell, examine a 3x3 "window" of cells around it,
+                 * and count the number of live (true) cells in the window. */
+                int live_in_window = 0;
+                for (int y_offset = -1; y_offset <= 1; y_offset += 1)
+                    for (int x_offset = -1; x_offset <= 1; x_offset += 1)
+                        if (LB_get(cc_args->args_state, x + x_offset, y + y_offset))
+                            live_in_window += 1;
 
+                /* Cells with 3 live neighbors remain or become live.
+                   Live cells with 2 live neighbors remain live. */
+                LB_set(cc_args->args_next_state, x, y,
+                    live_in_window == 3 /* dead cell with 3 neighbors or live cell with 2 */ ||
+                    (live_in_window == 4 && LB_get(cc_args->args_state, x, y)) /* live cell with 3 neighbors */
+                );
 
 		}
 	}
@@ -56,7 +71,7 @@ void simulate_life_parallel(int threads, LifeBoard *state, int steps) {
 				hold_height_start = args.height_end + 1;
 				hold_width_start = args.width_end + 1;
 			
-			}else{
+			} else{
 				
 				args.height_start = hold_height_start;
 				args.height_end = (hold_height_start + height_remainder) -1;
