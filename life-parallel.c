15c83dac86d2f2752c8896a5c19b1d28afcee2da
Sophie S <vpq5kd@virginia.edu>
2024-11-19 11:12:10 -0500

    fixed minor bugs from last commit

--- life-parallel.c
+++ life-parallel.c
@@ -6,6 +6,7 @@ typedef struct function_arguments{
 	int height_end;
 	int width_start;
 	int width_end;
+	int steps;
 	LifeBoard * args_state;
 	LifeBoard * args_next_state;
 	pthread_barrier_t * step_barrier;
@@ -14,9 +15,9 @@ typedef struct function_arguments{
 void * check_cells(void * args){
 
 	arguments * cc_args = (arguments *) args;
-	for (int step = 0; step < steps; step +=1 ){
+	for (int step = 0; step < cc_args->steps; step +=1 ){
 		for (int y = cc_args->height_start; y < cc_args->height_end ; y += 1) {
-			for (int x = 1; x < cc_args->state->widht-1; x += 1) {
+			for (int x = 1; x < cc_args->args_state->width-1; x += 1) {
 				 /* For each cell, examine a 3x3 "window" of cells around it,
 					 * and count the number of live (true) cells in the window. */
 					int live_in_window = 0;
@@ -31,12 +32,12 @@ void * check_cells(void * args){
 						live_in_window == 3 /* dead cell with 3 neighbors or live cell with 2 */ ||
 						(live_in_window == 4 && LB_get(cc_args->args_state, x, y)) /* live cell with 3 neighbors */
 					);
-
 			}
 		}
 		pthread_barrier_wait(cc_args->step_barrier);
-		LB_swap(cc_args->state, cc_args->next_state);
+		LB_swap(cc_args->args_state, cc_args->args_next_state);
 	}
+	return NULL;
 }
 
 void simulate_life_parallel(int threads, LifeBoard *state, int steps) {
@@ -76,6 +77,7 @@ void simulate_life_parallel(int threads, LifeBoard *state, int steps) {
 			args[thread_num].width_end = (hold_width_start + width_remainder) -1;
 		}
 
+		args[thread_num].steps = steps;
 		args[thread_num].args_state = state;
 		args[thread_num].args_next_state = next_state;
 		args[thread_num].step_barrier = &step_barrier;
