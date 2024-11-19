8a4fad6e3ef3dda1784625a9b3488dc163813096
Sophie S <vpq5kd@virginia.edu>
2024-11-18 21:50:25 -0500

    updated the helper function arguments to be passed as a signle struct

--- life-parallel.c
+++ life-parallel.c
@@ -1,22 +1,45 @@
 #include "life.h"
 #include <pthread.h>
 
-void check_cells(int  height_start, int height_end, int width_start, int width_end){
+void check_cells(void * args){
+	typdef struct function_arguments(){		
+		int height_start;
+		int height_end;
+		int width_start;
+		int width_end;
+	} arguments;
 
-	for (height_start; y < height_end ; y += 1) {
-    	for (width_start; x < state->width_end; x += 1) {
+	arguments * cc_args = (arguments *) args;
+
+	for (int y = cc_args->height_start; y < cc_args->height_end ; y += 1) {
+    	for (int x = cc_args->width_start; x < cc_args->width_end; x += 1) {
 
 
 		}
 	}
-};
+}
 
 void simulate_life_parallel(int threads, LifeBoard *state, int steps) {
     /* YOUR CODE HERE */
 	
-	pthread_t threads[threads];
+	pthread_t check_threads[threads];
 
 	LifeBoard *next_state = LB_new(state->width, state->height);
+	
+	int width_sep = state->width/threads;
+	int height_sep = state->height/threads;
+
+	int width_remainder = state->width%threads;
+	int height_remainder = state->height%threads;
+
+	typedef struct function_arguments{
+		int height_start;
+		int height_end;
+		int width_start;
+		int width_end;
+	} arguments;
+
+	
 	for (int step = 0; step < steps; step += 1){
 		
 	}
